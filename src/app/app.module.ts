db800fac6e843a25065b4c515f964be8f0ddcb94
Pascal ROUSSEL <kalnet17@gmail.com>
2019-09-19 10:49:07 +0200

    1er Exercice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, LOCALE_ID } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ArticleComponent } from "./article/article.component";
+import { FormsModule } from "@angular/forms";
+
+import { registerLocaleData } from "@angular/common";
+import localeFr from "@angular/common/locales/fr";
+
+registerLocaleData(localeFr, "fr");
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent, ArticleComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [[ { provide: LOCALE_ID, useValue: "fr-FR" } ]],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
