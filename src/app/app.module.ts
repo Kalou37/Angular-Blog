c71243282e5c5ad2c977c5d9188b2f8c0c08efd2
Pascal ROUSSEL <kalnet17@gmail.com>
2019-09-19 11:38:57 +0200

    Correctif sujet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,19 @@ import { NgModule, LOCALE_ID } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { ArticleComponent } from "./article/article.component";
+import { PostListComponent } from "./postList/postList.component";
 import { FormsModule } from "@angular/forms";
 
 import { registerLocaleData } from "@angular/common";
 import localeFr from "@angular/common/locales/fr";
+import { PostListItemComponent } from "./post-list-item/post-list-item.component";
 
 registerLocaleData(localeFr, "fr");
 
 @NgModule({
-  declarations: [AppComponent, ArticleComponent],
+  declarations: [AppComponent, PostListComponent, PostListItemComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [[ { provide: LOCALE_ID, useValue: "fr-FR" } ]],
+  providers: [[{ provide: LOCALE_ID, useValue: "fr-FR" }]],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: "app-article",
-  templateUrl: "./article.component.html",
-  styleUrls: ["./article.component.scss"]
+  selector: "app-post-list-item",
+  templateUrl: "./post-list-item.component.html",
+  styleUrls: ["./post-list-item.component.scss"]
 })
-export class ArticleComponent implements OnInit {
+export class PostListItemComponent implements OnInit {
   @Input() postTitle: string;
   @Input() postContent: string;
   @Input() postLoveIts: number;
@@ -24,13 +24,13 @@ export class ArticleComponent implements OnInit {
     if (this.postDate) {
       return this.postDate;
     } else {
-      let year = (Math.random() * 18) + 2000;
+      let year = Math.random() * 18 + 2000;
       let month = Math.random() * 11;
       let day = Math.random() * 28; // Pour éviter trop de code, je limite les jours à 28 (car février n'a que 28 jours)
       let hour = Math.random() * 23;
       let minute = Math.random() * 59;
       let second = Math.random() * 59;
-      return this.postDate = new Date(year, month, day, hour, minute, second);
+      return (this.postDate = new Date(year, month, day, hour, minute, second));
     }
   }
 
